8f389f39fe8604217f79f033a620a76ca5a838fa
Siddharta Govindaraj <siddharta@silverstripesoftware.in>
2023-03-24 19:26:56 +0530

    Added characterisation tests for card display

---
+++ tests/test_card_display.py
@@ -0,0 +1,24 @@
+from blackjack_r2ha_2022_python.card import Card
+from blackjack_r2ha_2022_python.rank import Rank
+from blackjack_r2ha_2022_python.suit import Suit
+
+
+def test_display_ten_as_string():
+    card = Card(Suit.DIAMONDS, Rank.TEN)
+    assert (card.display() == "\x1b[0;31;49m┌─────────┐"
+        + "\x1b[1B\x1b[11D│10       │"
+        + "\x1b[1B\x1b[11D│         │"
+        + "\x1b[1B\x1b[11D│    ♦    │"
+        + "\x1b[1B\x1b[11D│         │"
+        + "\x1b[1B\x1b[11D│       10│"
+        + "\x1b[1B\x1b[11D└─────────┘")
+
+def test_display_non_ten_as_string():
+    card = Card(Suit.CLUBS, Rank.ACE)
+    assert (card.display() == "\x1b[0;30;49m┌─────────┐"
+        + "\x1b[1B\x1b[11D│A        │"
+        + "\x1b[1B\x1b[11D│         │"
+        + "\x1b[1B\x1b[11D│    ♣    │"
+        + "\x1b[1B\x1b[11D│         │"
+        + "\x1b[1B\x1b[11D│        A│"
+        + "\x1b[1B\x1b[11D└─────────┘")
